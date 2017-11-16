baea3cb392a297f027f582f73a5334223d8b1f75
dcw102213006 <dcw102213006@gmail.com>
2017-11-16 22:41:30 +0800

    dcw102213006

--- index.php
+++ index.php
@@ -5,71 +5,25 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="index_style.css" rel="stylesheet" type="text/css">
+  <link href="css/index_style.css" rel="stylesheet" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	    <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
     <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
     <script src="/bundles/jquery?v=JzhfglzUfmVF2qo-weTo-kvXJ9AJvIRBLmu11PgpbVY1"></script>
 
-<style>
-div.gallery { /*圖片的外框大小*/
-    border: 1px solid #ccc;
-}
-
-div.gallery:hover {
-    border: 1px solid #777;
-}
-
-div.gallery img {
-    width: 100%;
-    height: auto;
-}
-
-div.desc { /* Add desc...區塊的大小*/
-    padding: 15px;
-    text-align: center;
-}
-
-* {
-    box-sizing: border-box;
-}
-
-.responsive {
-    padding: 0 6px;
-    float: left;
-    width: 30%;
-}
-
-@media only screen and (max-width: 700px){
-    .responsive {
-        width: 49.99999%;
-        margin: 6px 0;
-    }
-}
-
-@media only screen and (max-width: 500px){
-    .responsive {
-        width: 100%;
-    }
-}
-
-.clearfix:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-</style>
 </head>
 <body class="body">
 <?php
-
+  
   require 'Oracle_connect.php';
- 
+  
   if(!isset($_SESSION['uname'])){//若使用者尚未登入
 	  header('Location:login.php');
   }
   else{
+	   $uid=$_SESSION['uid'];
+		
 	   echo '<td align="center">'.$_SESSION['uname'].'您好!'.'<a href="logout.php" >登出</a>' ;
   }
 ?>
@@ -77,8 +31,8 @@ div.desc { /* Add desc...區塊的大小*/
   <aside class="side1" >
   	<h1 align="center">Let's Swap!</h1>
     
-                    <li class="secnav-right__profile file" >
-    <a href=""><span><img src="head.png" width="64" height="64" alt=""/></span></a>
+    <li class="secnav-right__profile file" >
+    <a href="profile.php?id=<?php echo $uid?>"><span><img src="head.png" width="64" height="64" alt=""/></span></a>
     <div class="subnav subnav--profile subnav--cart-file">
         <div class="subnav--profile__content">
             <div class="profile">
@@ -92,7 +46,7 @@ div.desc { /* Add desc...區塊的大小*/
         </div>
     </div>
     <!-- /subnav--cart -->
-</li>
+    </li>
 
   </aside>
   <div class="jumbotron type">
--- login.php
+++ login.php
@@ -1,7 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
-<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
 <?php
@@ -15,31 +17,36 @@ require 'Oracle_connect.php';
 if(  $uname  ){
     $uname="'".trim($uname)."'";
     $pwd="'".trim($pwd)."'";//強制將字串加上quote,Oracle預設將不加quote的字串強制轉成大寫字母,會引發sql的語句error
-    $sql ="select * from MEMBER where Account=$uname and Password=$pwd ";
+    
+	//sql:從MEMBER資料表檢查此帳號密碼是否存在
+	$sql ="select * from MEMBER where Account=$uname and Password=$pwd ";
     $stmt = OCIParse($db_link, $sql);
     if(!$stmt) {
         echo "<h1>ERROR – Could not parse SQL statement.</h1>";
         exit;
     }
-	try {
-        if(!@OCIExecute($stmt))throw new Exception();//例外事件,可能為使用者輸入惡意字串
+	else{
+	    try {
+            if(!@OCIExecute($stmt))throw new Exception();//例外事件,可能為使用者輸入惡意字串
 		
-		//以下為使用者輸入正常的字串
-		if($row = oci_fetch_row($stmt)) {
-    
-            $_SESSION['uname']=$row[1];
-			header('Location:index.php');
+	    	else{
+                //以下為使用者輸入正常的字串
+    	        if($row = oci_fetch_row($stmt)){
+				    $_SESSION['uname']=$row[1];
+					$_SESSION['uid']=$row[0];
+			        header('Location:index.php');
 		    
 		
+	            }
+	            else{
+		            echo '無此帳號!';
+	            }
+		    }
 	    }
-	    else{
-	    	echo '無此帳號!';
+	    catch(Exception $e){//SQL command not properly ended 
+		    echo '無此帳號!';
 	    }
-	}
-	catch(Exception $e){//SQL command not properly ended 
-		echo '無此帳號!';
-	}
-    
+    }
 }
 
 ?>
@@ -74,3 +81,169 @@ if(  $uname  ){
 </div>
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<?php
+header("Content-Type:text/html; charset=utf-8");
+$uname=isset($_POST['uname'])?$_POST['uname']:'';
+$pwd=isset($_POST['pwd'])?$_POST['pwd']:'';
+
+
+
+require 'Oracle_connect.php';
+if(  $uname  ){
+    $uname="'".trim($uname)."'";
+    $pwd="'".trim($pwd)."'";//強制將字串加上quote,Oracle預設將不加quote的字串強制轉成大寫字母,會引發sql的語句error
+    
+	//sql:從MEMBER資料表檢查此帳號密碼是否存在
+	$sql ="select * from MEMBER where Account=$uname and Password=$pwd ";
+    $stmt = OCIParse($db_link, $sql);
+    if(!$stmt) {
+        echo "<h1>ERROR – Could not parse SQL statement.</h1>";
+        exit;
+    }
+	else{
+	    try {
+            if(!@OCIExecute($stmt))throw new Exception();//例外事件,可能為使用者輸入惡意字串
+		
+	    	else{
+                //以下為使用者輸入正常的字串
+    	        if($row = oci_fetch_row($stmt)){
+				    $_SESSION['uname']=$row[1];
+					$_SESSION['uid']=$row[0];
+			        header('Location:index.php');
+		    
+		
+	            }
+	            else{
+		            echo '無此帳號!';
+	            }
+		    }
+	    }
+	    catch(Exception $e){//SQL command not properly ended 
+		    echo '無此帳號!';
+	    }
+    }
+}
+
+?>
+
+
+<body>
+<div id="content">
+<h2>Login Form</h2>
+
+<form action="login.php" method="Post">
+  <div class="banner">
+      <div class="fixed-bg">
+   
+      </div>
+  </div>
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" id="uname" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" id="pwd" placeholder="Enter Password" name="pwd" required>
+        
+    <button type="submit">Login</button>
+   
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+</div>
+</body>
+</html>
+>>>>>>> b836e0d8e157d567e689866cda14a110b9350d6e
+=======
+<!DOCTYPE html>
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<?php
+header("Content-Type:text/html; charset=utf-8");
+$uname=isset($_POST['uname'])?$_POST['uname']:'';
+$pwd=isset($_POST['pwd'])?$_POST['pwd']:'';
+
+
+
+require 'Oracle_connect.php';
+if(  $uname  ){
+    $uname="'".trim($uname)."'";
+    $pwd="'".trim($pwd)."'";//強制將字串加上quote,Oracle預設將不加quote的字串強制轉成大寫字母,會引發sql的語句error
+    
+	//sql:從MEMBER資料表檢查此帳號密碼是否存在
+	$sql ="select * from MEMBER where Account=$uname and Password=$pwd ";
+    $stmt = OCIParse($db_link, $sql);
+    if(!$stmt) {
+        echo "<h1>ERROR – Could not parse SQL statement.</h1>";
+        exit;
+    }
+	else{
+	    try {
+            if(!@OCIExecute($stmt))throw new Exception();//例外事件,可能為使用者輸入惡意字串
+		
+	    	else{
+                //以下為使用者輸入正常的字串
+    	        if($row = oci_fetch_row($stmt)){
+				    $_SESSION['uname']=$row[1];
+					$_SESSION['uid']=$row[0];
+			        header('Location:index.php');
+		    
+		
+	            }
+	            else{
+		            echo '無此帳號!';
+	            }
+		    }
+	    }
+	    catch(Exception $e){//SQL command not properly ended 
+		    echo '無此帳號!';
+	    }
+    }
+}
+
+?>
+
+
+<body>
+<div id="content">
+<h2>Login Form</h2>
+
+<form action="login.php" method="Post">
+  <div class="banner">
+      <div class="fixed-bg">
+   
+      </div>
+  </div>
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" id="uname" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" id="pwd" placeholder="Enter Password" name="pwd" required>
+        
+    <button type="submit">Login</button>
+   
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+</div>
+</body>
+</html>
+>>>>>>> b836e0d8e157d567e689866cda14a110b9350d6e
