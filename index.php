88b6652dbc32f3bcb7ba8e26c5b2c493a03f274c
zonaylc <33519861+zonaylc@users.noreply.github.com>
2017-11-18 02:18:46 +0800

    Add files via upload
    #剩下要從資料庫撈東西出來的php還沒放進去,因為不知道資料的名稱(迴圈跟有幾個人按讚的地方)
    #然後最右邊個人資料的設定應該沒錯但是顯示怪怪的所以等放圖片進去再試試看
    #如果有其他格式上的問題或什麼就邊做邊修吧!

--- index.php
+++ index.php
@@ -1,66 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>歡迎來到換物平台!</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="css/index_style.css" rel="stylesheet" type="text/css">
+  <link href="index_style.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	    <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
-    <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
-    <script src="/bundles/jquery?v=JzhfglzUfmVF2qo-weTo-kvXJ9AJvIRBLmu11PgpbVY1"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react.min.js"></script>
+  <script src="//f2e.cloudcdn.biz/cdn_lib/react@15.6.1/react-dom.min.js"></script>
 
+
+
+
+<style>
+div.gallery { /*圖片的外框大小*/
+    border: 5px solid #FFF;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc { /* Add desc...區塊的大小*/
+    padding: 15px;
+    text-align: center;
+	background-color: #FCFCFC
+}
+
+* {
+    box-sizing: border-box;
+}
+
+.responsive {
+    padding: 0 6px;
+    float: left;
+    width: 30%;
+}
+
+@media only screen and (max-width: 893px){
+    .responsive {
+        width: 49.99999%;
+        margin: 6px 0;
+    }
+}
+
+@media only screen and (max-width: 500px){
+    .responsive {
+        width: 100%;
+    }
+}
+
+.clearfix:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+</style>
 </head>
 <body class="body">
+<aside class="loginside" align="right">
 <?php
-  
+
   require 'Oracle_connect.php';
-  
+ 
   if(!isset($_SESSION['uname'])){//若使用者尚未登入
 	  header('Location:login.php');
   }
   else{
-	   $uid=$_SESSION['uid'];
-		
-	   echo '<td align="center">'.$_SESSION['uname'].'您好!'.'<a href="logout.php" >登出</a>' ;
+	   echo $_SESSION['uname'].'您好!'.'<a href="logout.php">登出</a>' ;
   }
 ?>
-<div class="container" align="right">
-  <aside class="side1" >
+</aside>
+
+<div class="container" align="center">
+  <aside class="side1" align="right">
   	<h1 align="center">Let's Swap!</h1>
     
-    <li class="secnav-right__profile file" >
-    <a href="profile.php?id=<?php echo $uid?>"><span><img src="head.png" width="64" height="64" alt=""/></span></a>
-    <div class="subnav subnav--profile subnav--cart-file">
-        <div class="subnav--profile__content">
-            <div class="profile">
-                <div class="nanoscroll__content">
-				<a class="subnav--cart__btn" href="">個人檔案</a>
-                </div>
-            </div>
-        </div>
-        <div class="subnav--cart__ft">
-            <a class="subnav--cart__btn" href="">我的願望清單</a>
+    <div class="w3-container" align="right">
+		<div class="w3-dropdown-hover">
+        <img class="w3-btn w3-circle w3-image w3-jumbo" src="img_fjords.jpg" >
+        <div class="w3-dropdown-content w3-bar-block w3-border">
+        <a href="#" class="w3-bar-item w3-button">個人檔案</a>
+        <a href="#" class="w3-bar-item w3-button">我的願望清單</a>
         </div>
     </div>
-    <!-- /subnav--cart -->
-    </li>
+</div>
 
   </aside>
-  <div class="jumbotron type">
+  
+  
+  <div class="container type" align="center">
   
     <div class="btn-group btype" role="group">
-      
       <div class="btn-group" role="group">
-        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle="dropdown" aria-expanded="false">服飾<span class="caret"></span></button>
+        <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B1" data-toggle		="dropdown" aria-expanded="false">服飾<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
           <li><a href="#">女生</a></li>
           <li><a href="#">男生</a></li>
         </ul>
       </div>
-            <div class="btn-group" role="group">
+      <div class="btn-group" role="group">
         <button id="btnDropdown1" type="button" class="btn btn-default dropdown-toggle B2" data-toggle="dropdown" aria-expanded="false">鞋類<span class="caret"></span></button>
         <ul class="dropdown-menu" role="menu" aria-labelledby="btnDropdown1">
           <li><a href="#">女生</a></li>
@@ -76,15 +123,17 @@
   
   
   <div id="wall">
-  
-  
+ 
   <div class="responsive">
   	  <div class="gallery">
     <a target="_blank" href="img_fjords.jpg">
       <img src="img_fjords.jpg" alt="Trolltunga Norway" width="256" height="256">
     </a>
     <div class="desc">Add a description of the image here <br>
-    <button type="button" class="btn btn-sm btn-default like"></button>
+    <button type="button" class="btn btn-sm btn-default like">
+ <!-- 統計回傳讚數  <? php
+	?>-->
+    
     </div>
  		</div>
 </div>
@@ -118,5 +167,7 @@
   
   </div>
 </div>
+
+
 </body>
 </html>
